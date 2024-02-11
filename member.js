5d88186194b286bf03e893113732bdeb8245cd3f
arrrr2 <113237338+arrrr2@users.noreply.github.com>
2024-02-11 04:18:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMumber() {
+  console.log('I am a member of the team');
+  console.log('I have the following skills:');
+  console.log('I can work in a team');
+  console.log('I can work with people');
+  console.log('I can work with computers');
+  console.log('I can work with data');
+  console.log('I can work with machines');
+  console.log('I can work with code');
+  console.log('I can work with numbers');
+  console.log('I can work with words');
+}
